ad7e6cf62c3cd7e5da02dc827fc35e6a585d746f
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-02-12 01:11:04 -0600

    fonts and colors

--- src/index.js
+++ src/index.js
@@ -5,17 +5,7 @@ import './_global.scss';
 const App = () => {
   return (
     <div className='MainApp'>
-      My first React Project!
-      <div className='row'>
-        <span>
-          <img
-            src='./assets/react-logo.png'
-            height='150'
-            width='150'
-            alt='react'
-          />
-        </span>
-      </div>
+      under construction
     </div>
   );
 };
